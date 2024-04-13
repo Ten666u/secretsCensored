48a78e49dc4e119e21bcf2da0c953a1e15dca44c
Maksim Bardin <bardin.maksim2@wildberries.work>
2024-04-13 15:16:48 +0300

    Добавление тестов

--- main.test.js
+++ main.test.js
@@ -1,5 +1,18 @@
 const { cleanText } = require('./main.js')
 
+test('Personal Information should be censored', () => {
+    expect(cleanText('Меня зовут Максим Бардин, я из Москвы, живу на улице Сеченого'))
+        .toBe('Меня зовут [censored] [censored], я из [censored], живу на улице [censored]')
+}) 
+
 test('Phone number should be censored', () => {
     expect(cleanText('8(910)-123-45-67')).toBe('[censored]')
+}) 
+
+test('Geographic coordinates be censored', () => {
+    expect(cleanText("Мои координаты 327°37'")).toBe('Мои координаты [censored]')
+}) 
+
+test('Postal code should be censored', () => {
+    expect(cleanText('Мой номер 8(910)-123-45-67')).toBe('Мой номер [censored]')
 }) 
